aab0231a918d15e1b0e8df3fe2aa8a47c8df43a9
Rod Hynes <rod-hynes@users.noreply.github.com>
2017-09-28 09:42:48 -0400

    Remove background task logic due to memory leak - performExpiringActivityWithReason appears to leak   CFStrings and NSMallocBlocks - over 300K leaks in 5 minutes under stress testing

--- PsiphonVPN/PacketTunnelProvider.m
+++ PsiphonVPN/PacketTunnelProvider.m
@@ -167,83 +167,26 @@
 }
 
 - (void)logMessage:(NSString * _Nonnull)message withTimestamp:(NSString * _Nonnull)timestamp{
-    // Called synchronously on PsiphonTunnel's callback queue.
-    // Any database or file operations should be performed
-    // synchronously before returning to guarantee ordering of
-    // database insertions, writes, etc.
-    [self performBackgroundTaskWithReason:@"onDiagnosticMessage" usingBlock:^{
-        [sharedDB insertDiagnosticMessage:message withTimestamp:timestamp];
+
+    // Certain aspects of the diagnostics currently depend on
+    // insert order, not timestamp order; for example, truncation.
+    // Since tunnel-core notices are dispatched synchronously to
+    // onDiagnostic on the callback queue, they will be inserted
+    // in timestamp order.
+
+    [sharedDB insertDiagnosticMessage:message withTimestamp:timestamp];
+
 #if DEBUG
-        // Notify container that there is new data in shared sqlite database.
-        // This is only needed in debug mode where the log view is enabled
-        // in the container. LogViewController needs to know when new logs
-        // have entered the shared database so it can update its view to
-        // display the latest diagnostic entries.
-        [notifier post:@"NE.onDiagnosticMessage"];
+
+    // Notify container that there is new data in shared sqlite database.
+    // This is only needed in debug mode where the log view is enabled
+    // in the container. LogViewController needs to know when new logs
+    // have entered the shared database so it can update its view to
+    // display the latest diagnostic entries.
+
+    [notifier post:@"NE.onDiagnosticMessage"];
+
 #endif
-    }];
-}
-
-
-/*
- * Execute block as a background task. The background task will end after your block argument returns. The reason string is used for debugging.
- *
- * This method will not return until the background task completes successfully or expires.
- * This method should be used as a wrapper for any code that can result in a lock being obtained on a file in the shared app group container.
- * This method must be used if the code could hold this manner of lock while the extension is being suspended.
- * If the extension holds a lock during suspension it will be terminated by the OS with exception code 0xdead10cc.
- */
-- (void)performBackgroundTaskWithReason:(NSString * _Nonnull)reason usingBlock:(void (^)(void))block {
-    // Follows template provided by "Requesting Background Assertions for Asynchronous Tasks" in:
-    // https://developer.apple.com/library/content/documentation/General/Conceptual/WatchKitProgrammingGuide/iOSSupport.html
-    dispatch_semaphore_t expiringActivityCompleted = dispatch_semaphore_create(0);
-    dispatch_semaphore_t blockExecutedOrActivityExpired = dispatch_semaphore_create(0);
-    [[NSProcessInfo processInfo] performExpiringActivityWithReason:reason usingBlock:^(BOOL expired) {
-        // This block is executed on an anonymous background queue.
-        // Note: this block can be called multiple times. If you are given
-        // a background task assertion, the block will be called with
-        // expiring == false. Then, if the app suddenly needs to terminate, the system can revoke
-        // the assertion by calling this block a second time with expiring == true.
-        if (expired) {
-            // Your app has not been given a background task assertion,
-            // or the current background task assertion has been revoked.
-            
-            // You have a brief bit of time to perform any clean-up activities.
-            
-            // Clean up any running tasks.
-            // Cancel any long synchronous tasks (if possible).
-            // Unblock the thread (if blocked).
-            dispatch_semaphore_signal(blockExecutedOrActivityExpired);
-            
-            // Allow performBackgroundTaskWithReason to return.
-            dispatch_semaphore_signal(expiringActivityCompleted);
-        } else {
-            // Your app has been given a background task assertion.
-            
-            // The background task assertion lasts until this method returns, or until
-            // the assertion is revoked.
-            
-            // perform long synchronous tasks here
-            // or kick off asynchronous task and block this thread.
-            // Be sure to unblock the thread when the asynchronous task is complete.
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                block();
-                dispatch_semaphore_signal(blockExecutedOrActivityExpired);
-            });
-            long timedOut = dispatch_semaphore_wait(blockExecutedOrActivityExpired, dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_SEC * 60));
-            if (timedOut == 0) {
-                // Async task completed successfully or the activity was expired.
-                LOG_DEBUG(@"onDiagnosticMessage: semaphore wait returned successfully");
-            } else {
-                LOG_DEBUG(@"onDiagnosticMessage: semaphore wait timed out");
-            }
-            
-            // Allow performBackgroundTaskWithReason to return.
-            dispatch_semaphore_signal(expiringActivityCompleted);
-        }
-    }];
-    
-    dispatch_semaphore_wait(expiringActivityCompleted, dispatch_time(DISPATCH_TIME_FOREVER, 0));
 }
 
 - (NSArray *)getNetworkInterfacesIPv4Addresses {
