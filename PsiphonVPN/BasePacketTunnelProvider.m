2dbeb3a8a78107a1a41a1b90fa2fa19fcf54ff37
Amir Khan <a.khan@psiphon.ca>
2018-10-15 15:47:29 -0400

    Add serial queue for NEPacketTunnelProvider system callbacks

--- PsiphonVPN/BasePacketTunnelProvider.m
+++ PsiphonVPN/BasePacketTunnelProvider.m
@@ -45,11 +45,15 @@ PsiFeedbackLogType const BasePacketTunnelProviderLogType = @"BasePacketTunnelPro
     // Pointer to startTunnelWithOptions completion handler.
     // NOTE: value is expected to be nil after completion handler has been called.
     void (^vpnStartCompletionHandler)(NSError *__nullable error);
+
+    // Serial queue.
+    dispatch_queue_t workQueue;
 }
 
 - (instancetype)init {
     self = [super init];
     if (self) {
+        workQueue = dispatch_queue_create("ca.psiphon.PsiphonVPN.baseWorkQueue", DISPATCH_QUEUE_SERIAL);
        _vpnStartedSignal = [RACReplaySubject replaySubjectWithCapacity:1];
        _sharedDB = [[PsiphonDataSharedDB alloc] initForAppGroupIdentifier:APP_GROUP_IDENTIFIER];
     }
@@ -62,121 +66,137 @@ PsiFeedbackLogType const BasePacketTunnelProviderLogType = @"BasePacketTunnelPro
 - (void)startTunnelWithOptions:(nullable NSDictionary<NSString *, NSObject *> *)options
              completionHandler:(void (^)(NSError *__nullable error))completionHandler {
 
-    // Determine if the extension jetsammed previously to this start.
-    BOOL previouslyJetsammed = [self.sharedDB getExtensionJetsammedBeforeStopFlag];
+    dispatch_sync(workQueue, ^{
 
-     // Sets the crash flag. This flag is reset when `stopTunnelWithReason:completionHandler:` is called.
-    [self.sharedDB setExtensionJetsammedBeforeStopFlag:TRUE];
+        // Determine if the extension jetsammed previously to this start.
+        BOOL previouslyJetsammed = [self.sharedDB getExtensionJetsammedBeforeStopFlag];
 
-    if (previouslyJetsammed) {
-        [self.sharedDB incrementJetsamCounter];
-        [PsiFeedbackLogger errorWithType:BasePacketTunnelProviderLogType
-                                    json:@{@"JetsamCount": @([self.sharedDB getJetsamCounter])}];
-    }
+         // Sets the crash flag. This flag is reset when `stopTunnelWithReason:completionHandler:` is called.
+        [self.sharedDB setExtensionJetsammedBeforeStopFlag:TRUE];
 
-    // Creates boot test file used for testing if device is unlocked since boot.
-    // A boot test file is a file with protection type NSFileProtectionCompleteUntilFirstUserAuthentication.
-    // NOTE: it is assumed that this file is first created while the device is in an unlocked state,
-    //       since file with such protection level cannot be created while device is still locked from boot.
-    if (![self createBootTestFile]) {
-        // Undefined behaviour wrt. Connect On Demand. Fail fast.
-        [PsiFeedbackLogger error:@"Failed to create/check for boot test file."];
-    }
-
-    // List of paths to downgrade file protection to NSFileProtectionNone. The list could contain files or directories.
-    NSArray<NSString *> *paths = @[
-      // Note that this directory is not accessible in the container.
-      [[[[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask] lastObject] path],
-      // Shared container, containing logs and other data.
-      [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:APP_GROUP_IDENTIFIER] path],
-    ];
-
-    // Set file protection of all files needed by the extension and Psiphon tunnel framework to NSFileProtectionNone.
-    // This is required in order for "Connect On Demand" to work.
-    if (![FileUtils downgradeFileProtectionToNone:paths withExceptions:@[ [self getBootTestFilePath] ]]) {
-        // Undefined behaviour wrt. Connect On Demand. Fail fast.
-        [PsiFeedbackLogger error:@"Failed to set file protection."];
-    }
-
-#if DEBUG
-    [FileUtils listDirectory:paths[0] resource:@"Library"];
-    [FileUtils listDirectory:paths[1] resource:@"Shared container"];
-#endif
-
-    // Determine how the extension was started.
-    // ExtensionStartMethodFromCrash priority should be after _FromBoot and _FromContainer.
-    //
-    if ([self isStartBootTestFileLocked]) {
-        self.extensionStartMethod = ExtensionStartMethodFromBoot;
-    } else if ([((NSString *)options[EXTENSION_OPTION_START_FROM_CONTAINER]) isEqualToString:EXTENSION_OPTION_TRUE]) {
-        self.extensionStartMethod = ExtensionStartMethodFromContainer;
-    } else if (previouslyJetsammed) {
-        self.extensionStartMethod = ExtensionStartMethodFromCrash;
-    } else {
-        self.extensionStartMethod = ExtensionStartMethodOther;
-    }
-
-    // Hold a reference to the completionHandler
-    vpnStartCompletionHandler = completionHandler;
-
-    // Start the tunnel.
-    [(id <BasePacketTunnelProviderProtocol>)self startTunnelWithErrorHandler:^(NSError *error) {
-        if (error) {
-            vpnStartCompletionHandler(error);
-            vpnStartCompletionHandler = nil;
+        if (previouslyJetsammed) {
+            [self.sharedDB incrementJetsamCounter];
+            [PsiFeedbackLogger errorWithType:BasePacketTunnelProviderLogType
+                                        json:@{@"JetsamCount": @([self.sharedDB getJetsamCounter])}];
         }
-    }];
+
+        // Creates boot test file used for testing if device is unlocked since boot.
+        // A boot test file is a file with protection type NSFileProtectionCompleteUntilFirstUserAuthentication.
+        // NOTE: it is assumed that this file is first created while the device is in an unlocked state,
+        //       since file with such protection level cannot be created while device is still locked from boot.
+        if (![self createBootTestFile]) {
+            // Undefined behaviour wrt. Connect On Demand. Fail fast.
+            [PsiFeedbackLogger error:@"Failed to create/check for boot test file."];
+        }
+
+        // List of paths to downgrade file protection to NSFileProtectionNone. The list could contain files or directories.
+        NSArray<NSString *> *paths = @[
+          // Note that this directory is not accessible in the container.
+          [[[[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask] lastObject] path],
+          // Shared container, containing logs and other data.
+          [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:APP_GROUP_IDENTIFIER] path],
+        ];
+
+        // Set file protection of all files needed by the extension and Psiphon tunnel framework to NSFileProtectionNone.
+        // This is required in order for "Connect On Demand" to work.
+        if (![FileUtils downgradeFileProtectionToNone:paths withExceptions:@[ [self getBootTestFilePath] ]]) {
+            // Undefined behaviour wrt. Connect On Demand. Fail fast.
+            [PsiFeedbackLogger error:@"Failed to set file protection."];
+        }
+
+    #if DEBUG
+        [FileUtils listDirectory:paths[0] resource:@"Library"];
+        [FileUtils listDirectory:paths[1] resource:@"Shared container"];
+    #endif
+
+        // Determine how the extension was started.
+        // ExtensionStartMethodFromCrash priority should be after _FromBoot and _FromContainer.
+        //
+        if ([self isStartBootTestFileLocked]) {
+            self.extensionStartMethod = ExtensionStartMethodFromBoot;
+        } else if ([((NSString *)options[EXTENSION_OPTION_START_FROM_CONTAINER]) isEqualToString:EXTENSION_OPTION_TRUE]) {
+            self.extensionStartMethod = ExtensionStartMethodFromContainer;
+        } else if (previouslyJetsammed) {
+            self.extensionStartMethod = ExtensionStartMethodFromCrash;
+        } else {
+            self.extensionStartMethod = ExtensionStartMethodOther;
+        }
+
+        // Hold a reference to the completionHandler
+        vpnStartCompletionHandler = completionHandler;
+
+        // Start the tunnel.
+        [(id <BasePacketTunnelProviderProtocol>)self startTunnelWithErrorHandler:^(NSError *error) {
+            if (error) {
+                vpnStartCompletionHandler(error);
+                vpnStartCompletionHandler = nil;
+            }
+        }];
+
+    });
 }
 
 /**
  * Subclasses should not override this method.
  */
 - (void)stopTunnelWithReason:(NEProviderStopReason)reason completionHandler:(void (^)(void))completionHandler {
+    dispatch_sync(workQueue, ^{
 
-    // Resets the extension crash flag, since the extension hasn't crashed until stop is called.
-    [self.sharedDB setExtensionJetsammedBeforeStopFlag:FALSE];
+        // Resets the extension crash flag, since the extension hasn't crashed until stop is called.
+        [self.sharedDB setExtensionJetsammedBeforeStopFlag:FALSE];
 
-    // Assumes stopTunnelWithReason called exactly once only after startTunnelWithOptions.completionHandler(nil)
-    if (vpnStartCompletionHandler) {
-        vpnStartCompletionHandler([NSError errorWithDomain:BasePsiphonTunnelErrorDomain
-                                                      code:BasePsiphonTunnelErrorStoppedBeforeConnected]);
-        vpnStartCompletionHandler = nil;
-    }
+        // Assumes stopTunnelWithReason called exactly once only after startTunnelWithOptions.completionHandler(nil)
+        if (vpnStartCompletionHandler) {
+            vpnStartCompletionHandler([NSError errorWithDomain:BasePsiphonTunnelErrorDomain
+                                                          code:BasePsiphonTunnelErrorStoppedBeforeConnected]);
+            vpnStartCompletionHandler = nil;
+        }
 
-    [(id <BasePacketTunnelProviderProtocol>)self stopTunnelWithReason:reason];
+        [(id <BasePacketTunnelProviderProtocol>)self stopTunnelWithReason:reason];
 
-    completionHandler();
+        completionHandler();
+    });
 }
 
 - (BOOL)startVPN {
-    if (vpnStartCompletionHandler) {
-        vpnStartCompletionHandler(nil);
-        vpnStartCompletionHandler = nil;
-        self.VPNStarted = TRUE;
 
-        [self.vpnStartedSignal sendNext:nil];
-        [self.vpnStartedSignal sendCompleted];
+    __block BOOL retValue;
 
-        return TRUE;
-    }
-    return FALSE;
+    dispatch_sync(workQueue, ^{
+        if (vpnStartCompletionHandler) {
+            vpnStartCompletionHandler(nil);
+            vpnStartCompletionHandler = nil;
+            self.VPNStarted = TRUE;
+
+            [self.vpnStartedSignal sendNext:nil];
+            [self.vpnStartedSignal sendCompleted];
+
+            retValue = TRUE;
+        }
+        retValue = FALSE;
+    });
+
+    return retValue;
 }
 
 - (void)exitGracefully {
 
-    // This is a manual exit and not a jetsam.
-    [self.sharedDB setExtensionJetsammedBeforeStopFlag:FALSE];
+    dispatch_sync(workQueue, ^{
 
-    // We want to only wait for a maximum of 5 seconds for the tunnel to stop.
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC),
-      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        // This is a manual exit and not a jetsam.
+        [self.sharedDB setExtensionJetsammedBeforeStopFlag:FALSE];
+
+        // We want to only wait for a maximum of 5 seconds for the tunnel to stop.
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 5 * NSEC_PER_SEC),
+          dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            exit(1);
+        });
+
+        // Try to gracefully shutdown the tunnel to free up server resources quicker.
+        [(id <BasePacketTunnelProviderProtocol>)self stopTunnelWithReason:NEProviderStopReasonNone];
+
         exit(1);
     });
-
-    // Try to gracefully shutdown the tunnel to free up server resources quicker.
-    [(id <BasePacketTunnelProviderProtocol>)self stopTunnelWithReason:NEProviderStopReasonNone];
-
-    exit(1);
 }
 
 #pragma mark - Handling app messages
@@ -185,30 +205,32 @@ PsiFeedbackLogType const BasePacketTunnelProviderLogType = @"BasePacketTunnelPro
 #define EXTENSION_RESP_FALSE_DATA [EXTENSION_RESP_FALSE dataUsingEncoding:NSUTF8StringEncoding]
 
 - (void)handleAppMessage:(NSData *)messageData completionHandler:(nullable void (^)(NSData *__nullable responseData))completionHandler {
+    dispatch_sync(workQueue, ^{
 
-    if (!completionHandler) {
-        return;
-    }
+        if (!completionHandler) {
+            return;
+        }
 
-    NSData *respData = nil;
-    NSString *query = [[NSString alloc] initWithData:messageData encoding:NSUTF8StringEncoding];
+        NSData *respData = nil;
+        NSString *query = [[NSString alloc] initWithData:messageData encoding:NSUTF8StringEncoding];
 
-    if ([EXTENSION_QUERY_IS_PROVIDER_ZOMBIE isEqualToString:query]) {
-        // If the Psiphon tunnel has been started when the extension was started
-        // responds with EXTENSION_RESP_TRUE, otherwise responds with EXTENSION_RESP_FALSE
-        respData = [(id <BasePacketTunnelProviderProtocol>)self isNEZombie] ? EXTENSION_RESP_TRUE_DATA : EXTENSION_RESP_FALSE_DATA;
+        if ([EXTENSION_QUERY_IS_PROVIDER_ZOMBIE isEqualToString:query]) {
+            // If the Psiphon tunnel has been started when the extension was started
+            // responds with EXTENSION_RESP_TRUE, otherwise responds with EXTENSION_RESP_FALSE
+            respData = [(id <BasePacketTunnelProviderProtocol>) self isNEZombie] ? EXTENSION_RESP_TRUE_DATA : EXTENSION_RESP_FALSE_DATA;
 
-    } else if ([EXTENSION_QUERY_IS_TUNNEL_CONNECTED isEqualToString:query]) {
-        respData = ([(id <BasePacketTunnelProviderProtocol>)self isTunnelConnected]) ? EXTENSION_RESP_TRUE_DATA : EXTENSION_RESP_FALSE_DATA;
-    }
+        } else if ([EXTENSION_QUERY_IS_TUNNEL_CONNECTED isEqualToString:query]) {
+            respData = ([(id <BasePacketTunnelProviderProtocol>) self isTunnelConnected]) ? EXTENSION_RESP_TRUE_DATA : EXTENSION_RESP_FALSE_DATA;
+        }
 
-    if (respData) {
-        completionHandler(respData);
-        return;
-    }
+        if (respData) {
+            completionHandler(respData);
+            return;
+        }
 
-    // If completionHandler is not nil, iOS expects it to always be executed.
-    completionHandler(messageData);
+        // If completionHandler is not nil, iOS expects it to always be executed.
+        completionHandler(messageData);
+    });
 }
 
 #pragma mark - Boot test
