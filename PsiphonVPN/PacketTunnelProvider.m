ae9ab20ab74b983cc5d94727dbd8e1222aa7c12d
Amir Khan <a.khan@psiphon.ca>
2018-10-23 14:26:08 -0400

    Remove killing extension for expired subscriptions
    - In turn also remove grace period timer.
    
    - Alerts for expired subscriptions have also been removed.

--- PsiphonVPN/PacketTunnelProvider.m
+++ PsiphonVPN/PacketTunnelProvider.m
@@ -68,16 +68,6 @@ typedef NS_ENUM(NSInteger, SubscriptionAuthorizationStatus) {
     SubscriptionAuthorizationStatusActiveOrEmpty
 };
 
-/** Extension's grace period state. */
-typedef NS_ENUM(NSInteger, GracePeriodState) {
-    /** @const GracePeriodStateInactive The extension is not in grace period. */
-    GracePeriodStateInactive,
-    /** @const GracePeriodActive Grace period is active, and the grace period timer will start after user presses Done on the grace period message shown to them. */
-    GracePeriodStateActive,
-    /** @const GracePeriodDone Grace period timer is done, subscription check needs to be performed to reset grace period state. */
-    GracePeriodStateDone
-};
-
 /** PacketTunnelProvider state */
 typedef NS_ENUM(NSInteger, TunnelProviderState) {
     /** @const TunnelProviderStateInit PacketTunnelProvider instance is initialized. */
@@ -102,8 +92,6 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
 // waitForContainerStartVPNCommand signals that the extension should wait for the container
 // before starting the VPN.
 @property (atomic) BOOL waitForContainerStartVPNCommand;
-
-@property (nonatomic) GracePeriodState gracePeriodState;
 
 @property (nonatomic, nonnull) PsiphonTunnel *psiphonTunnel;
 
@@ -156,7 +144,6 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
         _tunnelProviderState = TunnelProviderStateInit;
         _subscriptionCheckState = nil;
         _waitForContainerStartVPNCommand = FALSE;
-        _gracePeriodState = GracePeriodStateInactive;
         _suppliedContainerAuthorizationIDs = [NSSet set];
     }
     return self;
@@ -226,31 +213,12 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
     __weak PacketTunnelProvider *weakSelf = self;
 
     void (^handleExpiredSubscription)(void) = ^{
-
         PSIAssert([weakSelf.subscriptionCheckState isInProgress]);
+        [PsiFeedbackLogger infoWithType:SubscriptionCheckLogType message:@"authorization expired restarting tunnel"];
 
-        if (weakSelf.extensionStartMethod == ExtensionStartMethodFromContainer) {
-
-            [PsiFeedbackLogger infoWithType:SubscriptionCheckLogType message:@"authorization expired restarting tunnel"];
-
-            // Restarts the tunnel to re-connect with the correct sponsor ID.
-            [weakSelf.subscriptionCheckState setStateNotSubscribed];
-            [weakSelf reconnectWithConfig:weakSelf.cachedSponsorIDs.defaultSponsorId];
-
-        } else {
-
-            // subscriptionCheckState should not be changed from inProgress, until the grace period expires
-            // and another subscription check happens.
-
-            if (self.gracePeriodState == GracePeriodStateDone) {
-                [PsiFeedbackLogger infoWithType:SubscriptionCheckLogType message:@"grace period finished killing extension"];
-
-                [self killExtensionForExpiredSubscription];
-            } else {
-                [PsiFeedbackLogger infoWithType:SubscriptionCheckLogType message:@"authorization expired starting grace period"];
-                [self startGracePeriod];
-            }
-        }
+        // Restarts the tunnel to re-connect with the correct sponsor ID.
+        [weakSelf.subscriptionCheckState setStateNotSubscribed];
+        [weakSelf reconnectWithConfig:weakSelf.cachedSponsorIDs.defaultSponsorId];
     };
 
     // tunnelConnectedSignal is an infinite signal that emits an item whenever Psiphon tunnel is connected.
@@ -394,7 +362,7 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
 
               switch (result.error.code) {
                   case SubscriptionResultErrorInvalidReceipt:
-                      [weakSelf killExtensionForInvalidReceipt];
+                      [weakSelf exitForInvalidReceipt];
                       break;
 
                   case SubscriptionResultErrorExpired:
@@ -442,25 +410,16 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
               if (requestDate) {
                   if ([subscription hasActiveAuthorizationForDate:requestDate]
                     && ![subscription hasActiveAuthorizationForDate:[NSDate date]]) {
-                      [self killExtensionForBadClock];
+                      [self exitForBadClock];
                       return;
                   }
               }
 
               if (subscription.authorization) {
-
                   // New authorization was received from the subscription verifier server.
                   // Restarts the tunnel to connect with the new authorization only if it is different from
                   // the authorization in use by the tunnel.
                   if (![subscription.authorization.ID isEqualToString:currentActiveAuthorization]) {
-
-                      if (self.gracePeriodState == GracePeriodStateDone) {
-                          // Grace period has finished, and subscription check finished successfully
-                          // with a new authorization.
-                          // Resets grace period state.
-                          self.gracePeriodState = GracePeriodStateInactive;
-                      }
-
                       [weakSelf.subscriptionCheckState setStateSubscribed];
                       [weakSelf reconnectWithConfig:weakSelf.cachedSponsorIDs.subscriptionSponsorId];
                   }
@@ -615,7 +574,7 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
     });
 }
 
-- (void)displayMessageAndKillExtension:(NSString *)message {
+- (void)displayMessageAndExitGracefully:(NSString *)message {
 
     // If failed to display, retry in 60 seconds.
     const int64_t retryInterval = 60;
@@ -902,7 +861,7 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
     const int64_t intervalSec = 60; // Every minute.
 
     [self displayMessage:
-        NSLocalizedStringWithDefaultValue(@"CANNOT_START_TUNNEL_DUE_TO_SUBSCRIPTION", nil, [NSBundle mainBundle], @"Your Psiphon subscription has expired.\nSince you're not a subscriber or your subscription has expired, Psiphon can only be started from the Psiphon app.\n\nPlease open the Psiphon app.", @"Alert message informing user that their subscription has expired or that they're not a subscriber, therefore Psiphon can only be started from the Psiphon app. DO NOT translate 'Psiphon'.")
+        NSLocalizedStringWithDefaultValue(@"CANNOT_START_TUNNEL_DUE_TO_SUBSCRIPTION", nil, [NSBundle mainBundle], @"You don't have an active subscription.\nSince you're not a subscriber or your subscription has expired, Psiphon can only be started from the Psiphon app.\n\nPlease open the Psiphon app to start.", @"Alert message informing user that their subscription has expired or that they're not a subscriber, therefore Psiphon can only be started from the Psiphon app. DO NOT translate 'Psiphon'.")
        completionHandler:^(BOOL success) {
            // If the user dismisses the message, show the alert again in intervalSec seconds.
            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, intervalSec * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
@@ -911,54 +870,16 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
        }];
 }
 
-- (void)startGracePeriod {
-
-    if (self.gracePeriodState != GracePeriodStateInactive) {
-        return;
-    }
-
-    self.gracePeriodState = GracePeriodStateActive;
-
-#if DEBUG
-    int64_t gracePeriodSec = 2 * 60; // 2 minutes.
-#else
-    int64_t gracePeriodSec = 1 * 60 * 60;  // 1 hour.
-#endif
-    __weak PacketTunnelProvider *weakSelf = self;
-
-    // User doesn't have an active subscription. Notify them, after making sure they've checked
-    // the notification we will start an hour of extra grace period.
-    // NOTE: Waits for the user to acknowledge the message before starting the extra grace period.
-    [self displayMessage:NSLocalizedStringWithDefaultValue(@"SUBSCRIPTION_EXPIRED_WILL_KILL_TUNNEL", nil, [NSBundle mainBundle], @"Your Psiphon subscription has expired. Psiphon will stop automatically in an hour if subscription is not renewed. Open the Psiphon app to review your subscription to continue using premium features.", @"Alert message informing user that their subscription has expired, and that Psiphon will stop in an hour if subscription is not renewed. Do not translate 'Psiphon'.")
-       completionHandler:^(BOOL success) {
-
-           if (!success) {
-               [PsiFeedbackLogger error:@"iOS was unable to display subscription grace period starting message"];
-               return;
-           }
-
-           dispatch_after(dispatch_walltime(NULL, gracePeriodSec * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-               weakSelf.gracePeriodState = GracePeriodStateDone;
-               [self scheduleSubscriptionCheckWithRemoteCheckForced:FALSE];
-           });
-       }];
-}
-
-- (void)killExtensionForExpiredSubscription {
-    NSString *message = NSLocalizedStringWithDefaultValue(@"TUNNEL_KILLED", nil, [NSBundle mainBundle], @"Psiphon has been stopped automatically since your subscription has expired.", @"Alert message informing user that Psiphon has been stopped automatically since the subscription has expired. Do not translate 'Psiphon'.");
-    [self displayMessageAndKillExtension:message];
-}
-
-- (void)killExtensionForBadClock {
+- (void)exitForBadClock {
     [PsiFeedbackLogger errorWithType:ExitReasonLogType message:@"bad clock"];
     NSString *message = NSLocalizedStringWithDefaultValue(@"BAD_CLOCK_ALERT_MESSAGE", nil, [NSBundle mainBundle], @"We've detected the time on your device is out of sync with your time zone. Please update your clock settings and restart the app", @"Alert message informing user that the device clock needs to be updated with current time");
-    [self displayMessageAndKillExtension:message];
+    [self displayMessageAndExitGracefully:message];
 }
 
-- (void)killExtensionForInvalidReceipt {
+- (void)exitForInvalidReceipt {
     [PsiFeedbackLogger errorWithType:ExitReasonLogType message:@"invalid subscription receipt"];
     NSString *message = NSLocalizedStringWithDefaultValue(@"BAD_RECEIPT_ALERT_MESSAGE", nil, [NSBundle mainBundle], @"Your subscription receipt can not be verified, please refresh it and try again.", @"Alert message informing user that subscription receipt can not be verified");
-    [self displayMessageAndKillExtension:message];
+    [self displayMessageAndExitGracefully:message];
 }
 
 - (void)displayCorruptSettingsFileMessage {
@@ -1115,7 +1036,7 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
             if (serverTimestamp != nil) {
                 if (![subscription hasActiveAuthorizationForDate:serverTimestamp]) {
                     // User is possibly cheating, terminate extension due to 'Bad Clock'.
-                    [self killExtensionForBadClock];
+                    [self exitForBadClock];
                 }
             }
         }
@@ -1133,7 +1054,7 @@ typedef NS_ENUM(NSInteger, TunnelProviderState) {
     if (selectedRegion && ![selectedRegion isEqualToString:kPsiphonRegionBestPerformance] && ![regions containsObject:selectedRegion]) {
         [[PsiphonConfigUserDefaults sharedInstance] setEgressRegion:kPsiphonRegionBestPerformance];
 
-        [self displayMessageAndKillExtension:NSLocalizedStringWithDefaultValue(@"VPN_START_FAIL_REGION_INVALID_MESSAGE", nil, [NSBundle mainBundle], @"The region you selected is no longer available. You must choose a new region or change to the default \"Best performance\" choice.", @"Alert dialog message informing the user that an error occurred while starting Psiphon because they selected an egress region that is no longer available (Do not translate 'Psiphon'). The user should select a different region and try again. Note: the backslash before each quotation mark should be left as is for formatting.")];
+        [self displayMessageAndExitGracefully:NSLocalizedStringWithDefaultValue(@"VPN_START_FAIL_REGION_INVALID_MESSAGE", nil, [NSBundle mainBundle], @"The region you selected is no longer available. You must choose a new region or change to the default \"Best performance\" choice.", @"Alert dialog message informing the user that an error occurred while starting Psiphon because they selected an egress region that is no longer available (Do not translate 'Psiphon'). The user should select a different region and try again. Note: the backslash before each quotation mark should be left as is for formatting.")];
     }
 }
 
