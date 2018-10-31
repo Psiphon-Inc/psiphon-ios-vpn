f5fd2ee7aeec50267e158a45e2f80ec9c297bf8f
Amir Khan <a.khan@psiphon.ca>
2018-10-30 20:02:24 -0400

    Added network connectivity notifications to Notifier
    - Also added notification observing through signals

--- Shared/Notifier.m
+++ Shared/Notifier.m
@@ -21,6 +21,7 @@
 #import "Notifier.h"
 #import "Asserts.h"
 #import "DispatchUtils.h"
+#import "RACSubject.h"
 
 #define PSIPHON_GROUP      @"group.ca.psiphon.Psiphon"
 #define PSIPHON_VPN_GROUP  @"group.ca.psiphon.Psiphon.PsiphonVPN"
@@ -34,7 +35,8 @@ NotifierMessage const NotifierNewHomepages           = PSIPHON_VPN_GROUP @".NewH
 NotifierMessage const NotifierTunnelConnected        = PSIPHON_VPN_GROUP @".TunnelConnected";
 NotifierMessage const NotifierAvailableEgressRegions = PSIPHON_VPN_GROUP @".AvailableEgressRegions";
 NotifierMessage const NotifierMarkedAuthorizations   = PSIPHON_VPN_GROUP @".MarkedAuthorizations";
-NotifierMessage const NotifierWaitingForNetworkConnectivity = PSIPHON_VPN_GROUP @".WaitingForNetworkConnectivity";
+NotifierMessage const NotifierNetworkConnectivityFailed = PSIPHON_VPN_GROUP @".NetworkConnectivityFailed";
+NotifierMessage const NotifierNetworkConnectivityResolved = PSIPHON_VPN_GROUP @".NetworkConnectivityResolved";
 
 // Messages sent by the container.
 NotifierMessage const NotifierStartVPN               = PSIPHON_GROUP @".StartVPN";
@@ -65,6 +67,8 @@ NotifierMessage const NotifierDebugPsiphonTunnelState = PSIPHON_GROUP @".DebugPs
 
 @implementation Notifier {
     NSMutableArray<ObserverTuple *> *observers;
+
+    RACSubject<NotifierMessage> *messagesSubject;
 }
 
 // Class variables accessible by C functions.
@@ -75,7 +79,7 @@ static void cfNotificationCallback(CFNotificationCenterRef center, void *observe
 
     NSString *key = (__bridge NSString *)name;
     Notifier *selfPtr = (__bridge Notifier *)observer;
-    [selfPtr notificationCallback:key];
+    [selfPtr notificationCallback:(NotifierMessage)key];
 }
 
 static inline void AddDarwinNotifyObserver(CFNotificationCenterRef center, const void *observer, CFStringRef key) {
@@ -89,6 +93,8 @@ static inline void AddDarwinNotifyObserver(CFNotificationCenterRef center, const
 
 - (instancetype)init {
     observers = [NSMutableArray arrayWithCapacity:1];
+
+    messagesSubject = [RACSubject subject];
 
     // Add self to Darwin notify center for the given key.
     CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
@@ -116,7 +122,8 @@ static inline void AddDarwinNotifyObserver(CFNotificationCenterRef center, const
     AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierTunnelConnected);
     AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierAvailableEgressRegions);
     AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierMarkedAuthorizations);
-    AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierWaitingForNetworkConnectivity);
+    AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierNetworkConnectivityFailed);
+    AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierNetworkConnectivityResolved);
 
 #if DEBUG
     AddDarwinNotifyObserver(center, (__bridge const void *)self, (__bridge CFStringRef)NotifierDebugPsiphonTunnelState);
@@ -135,6 +142,19 @@ static inline void AddDarwinNotifyObserver(CFNotificationCenterRef center, const
         sharedInstance = [[Notifier alloc] init];
     });
     return sharedInstance;
+}
+
+- (void)post:(NotifierMessage)message {
+
+    dispatch_async_main(^{
+        CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
+        if (center) {
+            CFNotificationCenterPostNotification(center, (__bridge CFStringRef)message, NULL, NULL, 0);
+
+            [PsiFeedbackLogger infoWithType:NotifierLogType message:@"sent [%@]", message];
+        }
+    });
+
 }
 
 - (void)registerObserver:(id <NotifierObserver>)observer callbackQueue:(dispatch_queue_t)queue {
@@ -159,26 +179,27 @@ static inline void AddDarwinNotifyObserver(CFNotificationCenterRef center, const
     }
 }
 
-- (void)post:(NotifierMessage)message {
-
-    dispatch_async_main(^{
-        CFNotificationCenterRef center = CFNotificationCenterGetDarwinNotifyCenter();
-        if (center) {
-            CFNotificationCenterPostNotification(center, (__bridge CFStringRef)message, NULL, NULL, 0);
-
-            [PsiFeedbackLogger infoWithType:NotifierLogType message:@"sent [%@]", message];
-        }
-    });
-
+- (RACSignal<NotifierMessage> *)listenForMessages:(NSArray<NotifierMessage> *)messages {
+    return [messagesSubject filter:^BOOL(NotifierMessage received) {
+        return [messages containsObject:received];
+    }];
 }
 
 #pragma mark - Private
 
-- (void)notificationCallback:(NSString *)message {
+// Called on the main thread.
+- (void)notificationCallback:(NotifierMessage)message {
 
     [PsiFeedbackLogger infoWithType:NotifierLogType message:@"received [%@]", message];
 
+    // Since subscribers could potentially block the main thread, we will not block the main
+    // thread to send the message to `messageSubject`.
+    dispatch_async_global(^{
+        [messagesSubject sendNext:message];
+    });
+
     @synchronized (sharedInstance) {
+
 
         NSMutableIndexSet *deallocatedDelegates = [NSMutableIndexSet indexSet];
 
