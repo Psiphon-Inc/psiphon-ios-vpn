afb19040bf2d51a2684d0bbfc3016a771b9e3131
Amir Khan <a.khan@psiphon.ca>
2018-11-15 17:16:36 -0500

    Move VPN permission alert into AlertDialogs class

--- Psiphon/ViewControllers/MainViewController.m
+++ Psiphon/ViewControllers/MainViewController.m
@@ -64,6 +64,7 @@
 #import "VPNStartAndStopButton.h"
 #import "UIView+AutoLayoutViewGroup.h"
 #import "OnboardingViewController.h"
+#import "AlertDialogs.h"
 
 PsiFeedbackLogType const RewardedVideoLogType = @"RewardedVideo";
 
@@ -220,31 +221,9 @@ UserDefaultsKey const PsiCashHasBeenOnboardedBoolKey = @"PsiCash.HasBeenOnboarde
           }
 
           if (startStatus == VPNStartStatusFailedUserPermissionDenied) {
-              
-              // Alert the user that their permission is required in order to install the VPN configuration.
-              UIAlertController *alert = [UIAlertController alertControllerWithTitle:NSLocalizedStringWithDefaultValue(@"VPN_START_PERMISSION_REQUIRED_TITLE", nil, [NSBundle mainBundle], @"Permission required", @"Alert dialog title indicating to the user that Psiphon needs their permission")
-                                                                             message:NSLocalizedStringWithDefaultValue(@"VPN_START_PERMISSION_DENIED_MESSAGE", nil, [NSBundle mainBundle], @"Psiphon needs your permission to install a VPN profile in order to connect.\n\nPsiphon is committed to protecting the privacy of our users. You can review our privacy policy by tapping \"Privacy Policy\".", @"('Privacy Policy' should be the same translation as privacy policy button VPN_START_PRIVACY_POLICY_BUTTON), (Do not translate 'VPN profile'), (Do not translate 'Psiphon')")
-                                                                      preferredStyle:UIAlertControllerStyleAlert];
 
-              UIAlertAction *privacyPolicyAction = [UIAlertAction actionWithTitle:NSLocalizedStringWithDefaultValue(@"VPN_START_PRIVACY_POLICY_BUTTON", nil, [NSBundle mainBundle], @"Privacy Policy", @"Button label taking user's to our Privacy Policy page")
-                  style:UIAlertActionStyleDefault
-                handler:^(UIAlertAction *action) {
-                    NSString *urlString = NSLocalizedStringWithDefaultValue(@"PRIVACY_POLICY_URL", nil, [PsiphonClientCommonLibraryHelpers commonLibraryBundle], @"https://psiphon.ca/en/privacy.html", @"External link to the privacy policy page. Please update this with the correct language specific link (if available) e.g. https://psiphon.ca/fr/privacy.html for french.");
-                    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]
-                       options:@{}
-                       completionHandler:^(BOOL success) {
-                        // Do nothing.
-                    }];
-                }];
-
-              UIAlertAction *dismissAction = [UIAlertAction actionWithTitle:NSLocalizedStringWithDefaultValue(@"VPN_START_PERMISSION_DISMISS_BUTTON", nil, [NSBundle mainBundle], @"Dismiss", @"Dismiss button title. Dismisses pop-up alert when the user clicks on the button")
-                                                                      style:UIAlertActionStyleCancel
-                                                                    handler:^(UIAlertAction *action) {
-                                                                        // Do nothing.
-                                                                    }];
-
-              [alert addAction:privacyPolicyAction];
-              [alert addAction:dismissAction];
+              // Present the VPN permission denied alert.
+              UIAlertController *alert = [AlertDialogs vpnPermissionDeniedAlert];
               [alert presentFromTopController];
 
           } else if (startStatus == VPNStartStatusFailedOther) {
