1ad8cf1e9d281fdd9742b41d3ad256cb1a1c4ccc
Amir Khan <a.khan@psiphon.ca>
2019-01-28 15:26:27 -0500

    Add PsiCash reward activity data update signal

--- Psiphon/PsiCash/ClientWrapper/PsiCashClient.h
+++ Psiphon/PsiCash/ClientWrapper/PsiCashClient.h
@@ -27,6 +27,12 @@
 
 @property (nonatomic, readonly) RACReplaySubject<PsiCashClientModel *> *clientModelSignal;
 
+/**
+ * Hot infinite stream of rewarded activity data package.
+ * Emits nil if no data has been set yet by the PsiCash library, otherwise emits the data package as a string.
+ */
+@property (nonatomic, readonly) RACBehaviorSubject<NSString *> *rewardedActivityDataSignal;
+
 + (instancetype)sharedInstance;
 
 #pragma mark - Data
