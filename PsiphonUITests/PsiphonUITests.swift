5b5884ccbee19b4bb46ddf3fe1236c3227df77b2
mirokuratczyk <miro.kuratczyk@gmail.com>
2019-05-02 11:44:14 -0400

    Fastlane integration for screenshots
    Deprecated helper script for generating and uploading
    screenshots in favour of Fastlane.

--- PsiphonUITests/PsiphonUITests.swift
+++ PsiphonUITests/PsiphonUITests.swift
@@ -53,13 +53,18 @@ class PsiphonUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         snapshot("main")
-        XCUIApplication().buttons["settings"].tap()
-        snapshot("settings")
-        XCUIApplication().cells.element(boundBy: 2).tap() // Region
+
+        XCUIApplication().buttons["regionSelectionButton"].tap() // Go to region selection
         snapshot("settings-region")
-        XCUIApplication().navigationBars.buttons.element(boundBy: 0).tap()
-        XCUIApplication().cells.element(boundBy: 3).tap() // Language
+        XCUIApplication().navigationBars.buttons.element(boundBy: 0).tap() // Back to main screen
+
+
+        XCUIApplication().buttons["settings"].tap() // Go to settings
+        snapshot("settings")
+
+        XCUIApplication().cells.element(boundBy: 3).tap() // Go to language selection screen
         snapshot("settings-language")
-        XCUIApplication().navigationBars.buttons.element(boundBy: 0).tap()
+
+        XCUIApplication().navigationBars.buttons.element(boundBy: 0).tap() // Back to main screen
     }
 }
