a04eac0b59bea0c2b9cce6ffff078efe14e0f7e9
Amir Khan <a.khan@psiphon.ca>
2020-03-20 17:54:21 -0400

    Check tunnel state immediately before opening landing page

--- Psiphon/URLHandler.swift
+++ Psiphon/URLHandler.swift
@@ -32,15 +32,16 @@ extension URLHandler {
                     observer.fulfill(value: true)
                     return
                 }
-                /// Due to memory pressure, the network extension is at high risk of jetsamming before the landing page can be opened.
-                /// Tunnel status should be assessed directly (not through observables that might introduce some latency),
-                /// before opening the landing page.
-                guard let landingPage = url.getValue(Current) else {
-                    observer.fulfill(value: false)
-                    return
-                }
 
                 DispatchQueue.main.async {
+                    /// Due to memory pressure, the network extension is at high risk of jetsamming before the landing page can be opened.
+                    /// Tunnel status should be assessed directly (not through observables that might introduce some latency),
+                    /// before opening the landing page.
+                    guard let landingPage = url.getValue(Current) else {
+                        observer.fulfill(value: false)
+                        return
+                    }
+                    
                     UIApplication.shared.open(landingPage) { success in
                         observer.fulfill(value: success)
                     }
