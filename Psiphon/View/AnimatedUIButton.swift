8c5b0f66540c8a141d2756fcd304ebe99696c767
Amir Khan <a.khan@psiphon.ca>
2020-03-21 01:47:09 -0400

    Add animated UIView on tap
    Motivation:
    Make UIView that respond like a button,
    also animate like `AnimatedUIButton`.
    
    Modifications:
    - Abstracted animation logic out of `AnimatedUIButton`
      into `Animations.swift`
    - Created `AnimatedUIButton`
    
    Result:
    Enables UIView to animate like `AnimatedUIButton`.

--- Psiphon/View/AnimatedUIButton.swift
+++ Psiphon/View/AnimatedUIButton.swift
@@ -23,29 +23,17 @@ class AnimatedUIButton: SwiftUIButton {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-
-        UIView.animate(withDuration: 0.1) {
-            self.transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
-            self.alpha = 0.9
-        }
+        animateShrink(.shrink, self)
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
-
-        UIView.animate(withDuration: 0.1) {
-            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            self.alpha = 1.0
-        }
+        animateShrink(.restore, self)
     }
 
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
-
-        UIView.animate(withDuration: 0.1) {
-            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-            self.alpha = 1.0
-        }
+        animateShrink(.restore, self)
     }
 
 }
