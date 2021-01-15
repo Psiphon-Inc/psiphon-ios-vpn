f88161b3748278fa165776cf482f4bcc3e790aa1
Amir Khan <a.khan@psiphon.ca>
2021-01-15 12:50:40 -0500

    Update clientPlatform string for iOS app running on Mac

--- Shared/AppInfo.m
+++ Shared/AppInfo.m
@@ -72,22 +72,49 @@ UserDefaultsKey const AppInfoFastLaneSnapShotBoolKey = @"FASTLANE_SNAPSHOT";
     //   - spaces, which are considered invalid by the server
     // Like "iOS". Older iOS reports "iPhone OS", which we will convert.
     NSString *systemName = [[UIDevice currentDevice] systemName];
+    
     if ([systemName isEqual: @"iPhone OS"]) {
         systemName = @"iOS";
     }
     systemName = [[systemName
                    stringByReplacingOccurrencesOfString:@"_" withString:@"-"]
                   stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+    
     // Like "10.2.1"
     NSString *systemVersion = [[[[UIDevice currentDevice]systemVersion]
                                 stringByReplacingOccurrencesOfString:@"_" withString:@"-"]
                                stringByReplacingOccurrencesOfString:@" " withString:@"-"];
 
-    // "unjailbroken"/"jailbroken"
-    NSString *jailbroken = @"unjailbroken";
-    if ([JailbreakCheck isDeviceJailbroken]) {
-        jailbroken = @"jailbroken";
+    // The value of this property is YES only when the process is an iOS app running on a Mac.
+    // The value of the property is NO for all other apps on the Mac, including Mac apps built
+    // using Mac Catalyst. The property is also NO for processes running on platforms other than macOS.
+    BOOL isiOSAppOnMac = FALSE;
+    if (@available(iOS 14.0, *)) {
+         isiOSAppOnMac = [[NSProcessInfo processInfo] isiOSAppOnMac];
     }
+    
+    // The value of this property is true when the process is:
+    // - A Mac app built with Mac Catalyst, or an iOS app running on Apple silicon.
+    // - Running on a Mac.
+    BOOL isMacCatalystApp = FALSE;
+    if (@available(iOS 14.0, *)) {
+        isMacCatalystApp = [[NSProcessInfo processInfo] isMacCatalystApp];
+    }
+    
+    // "unjailbroken"/"jailbroken/iOSAppOnMac/MacCatalystApp"
+    NSString *detail = @"unjailbroken";
+    
+    if (isiOSAppOnMac == TRUE && isMacCatalystApp == TRUE) {
+        detail = @"iOSAppOnMac";
+    } else if (isiOSAppOnMac == FALSE && isMacCatalystApp == TRUE) {
+        detail = @"MacCatalystApp";
+    } else {
+        // App is an iOS app running on iOS.
+        if ([JailbreakCheck isDeviceJailbroken]) {
+            detail = @"jailbroken";
+        }
+    }
+    
     // Like "com.psiphon3.browser"
     NSString *bundleIdentifier = [[[[NSBundle mainBundle] bundleIdentifier]
                                    stringByReplacingOccurrencesOfString:@"_" withString:@"-"]
@@ -96,8 +123,9 @@ UserDefaultsKey const AppInfoFastLaneSnapShotBoolKey = @"FASTLANE_SNAPSHOT";
     NSString *clientPlatform = [NSString stringWithFormat:@"%@_%@_%@_%@",
                                 systemName,
                                 systemVersion,
-                                jailbroken,
+                                detail,
                                 bundleIdentifier];
+    
     return clientPlatform;
 }
 
