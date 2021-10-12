5a54497d2dea8e21227a6f2acbd94dfaf2ad2e76
Amir Khan <a.khan@psiphon.ca>
2021-10-12 15:15:04 -0400

    Revert "TEST: Hardcoded landing page used for testing"
    This reverts commit 94fb8e2114a9e30f3453735d0aa609855f18b545.

--- Psiphon/LandingPage.swift
+++ Psiphon/LandingPage.swift
@@ -76,8 +76,7 @@ let landingPageReducer = Reducer<LandingPageReducerState
         // Hard-coded landing page for PsiCash accounts testing
         let randomlySelectedURL = URL(string: "https://landing.dev.psi.cash/dev-index.html")!
         #else
-        // let randomlySelectedURL = landingPages.randomElement()!.url
-        let randomlySelectedURL = URL(string: "https://landing.psi.cash")!
+        let randomlySelectedURL = landingPages.randomElement()!.url
         #endif
         
         return [
