e493985c23534de76923ae76156b8b62bc6727f3
Amir Khan <a.khan@psiphon.ca>
2021-10-22 17:02:13 -0400

    Updated AppStateParser dependencies

--- dev/AppStateParser/Package.swift
+++ dev/AppStateParser/Package.swift
@@ -16,9 +16,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/davedufresne/SwiftParsec.git", from: "4.0.1"),
-        .package(url: "https://github.com/mxcl/Chalk.git", from: "0.1.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.0")
+        .package(url: "https://github.com/mxcl/Chalk.git", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.1"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
