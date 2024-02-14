3baeacebc86deb15caf3c69b01be602b8b7ac24e
mirokuratczyk <miro.kuratczyk@gmail.com>
2024-02-14 10:43:27 -0500

    Accurate memory profiling on iOS 13+
    Remove old, inaccurate, memory profiling from iOS < 13

--- Shared/Profiling/AppStats.h
+++ Shared/Profiling/AppStats.h
@@ -24,31 +24,10 @@
  */
 @interface AppStats : NSObject
 
-
 /**
  * Gets size of each memory page in bytes.
  * @return Memory page size.
  */
 + (vm_size_t)pageSize:(NSError *_Nullable *_Nonnull)error;
-
-/**
- * Returns current mach tasks's resident set size.
- *
- * @param e A pointer to an error object. If an error occures, it will be set to
- * an error object describing the error. Otherwise, set to nil.
- *
- * @return Resident set size.
- */
-+ (mach_vm_size_t)residentSetSize:(NSError *_Nullable*_Nonnull)e;
-
-/**
- * Returns current mach task's private resident set size.
- *
- * @param e A pointer to an error object. If an error occures, it will be set to
- * an error object describing the error. Otherwise, set to nil.
- *
- * @return Private resident set size.
- */
-+ (size_t)privateResidentSetSize:(NSError *_Nullable*_Nonnull)e;
 
 @end
